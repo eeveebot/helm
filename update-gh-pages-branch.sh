704a2df63819ff908695843deff6ebba0696cd81
Edwin Pers <pers.edwin@gmail.com>
2025-08-24 15:17:37 -0400

    guh

--- update-gh-pages-branch.sh
+++ update-gh-pages-branch.sh
@@ -7,7 +7,8 @@ DIRNAME=$(dirname "$0")
 SCRIPT_DIR=$(cd "$DIRNAME" || exit 1; pwd)
 cd "$SCRIPT_DIR" || exit 1
 
-git switch gh-pages \
+git fetch \
+  && git switch gh-pages \
   && git pull \
   && git rebase main \
   && git push --force \
