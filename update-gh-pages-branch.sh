7ae8a4099c83cbbff4c0a5bd37e458d6807564a3
Edwin Pers <pers.edwin@gmail.com>
2025-08-24 15:15:03 -0400

    so fragile

--- update-gh-pages-branch.sh
+++ update-gh-pages-branch.sh
@@ -7,4 +7,8 @@ DIRNAME=$(dirname "$0")
 SCRIPT_DIR=$(cd "$DIRNAME" || exit 1; pwd)
 cd "$SCRIPT_DIR" || exit 1
 
-git switch gh-pages && git rebase main && git push --force && git switch main
+git switch gh-pages \
+  && git pull \
+  && git rebase main \
+  && git push --force \
+  && git switch main
