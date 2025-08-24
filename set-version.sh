5e39dd3e249fc5c4cbee8637e5b0faf92566880f
Edwin Pers <pers.edwin@gmail.com>
2025-08-24 15:38:39 -0400

    0.0.24-prep - build subcharts

--- set-version.sh
+++ set-version.sh
@@ -11,13 +11,10 @@ VERSION=$1
 
 export VERSION
 
-# Use find to locate all Chart.yaml files recursively
+# Find all Chart.yaml files recursively and update the version
 find . -name "Chart.yaml" | while IFS= read -r file; do
   echo "Updating $file to version $VERSION"
-
-  # Update the version field and dependencies in each document
-  yq e -i '(.. | select(tag == "!!map" and has("version"))).version = env(VERSION)' "$file"
-  # yq e -i '(.. | select(tag == "!!map" and has("dependencies"))).dependencies[] |= select(has("version")).version = env(VERSION)' "$file"
+  yq e -i '(.version) = env(VERSION)' "$file"
 done
 
 echo "Updated version to $VERSION in all Chart.yaml files."
