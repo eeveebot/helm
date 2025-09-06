1758d9076068ebe65750bb4aebc780dba6ac5473
Edwin Pers <pers.edwin@gmail.com>
2025-09-06 15:06:11 -0400

    update set-version script

--- set-version.sh
+++ set-version.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# where this .sh file lives
+DIRNAME=$(dirname "$0")
+SCRIPT_DIR=$(cd "$DIRNAME" || exit 1; pwd)
+cd "$SCRIPT_DIR" || exit 1
+
 # Check if a version argument is provided
 if [ -z "$1" ]; then
   echo "Usage: $0 <version>"
