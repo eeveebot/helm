0fa7adf728a5794762e029537c182bb56feebf34
Edwin Pers <pers.edwin@gmail.com>
2026-03-09 20:54:05 -0400

    time for e2e test

--- ci.sh
+++ ci.sh
@@ -10,57 +10,120 @@ DIRNAME=$(dirname "$0")
 SCRIPT_DIR=$(cd "$DIRNAME" || exit 1; pwd)
 cd "$SCRIPT_DIR" || exit 1
 
-# git setup
-echo "Setup git"
-git config --global --add safe.directory "$(pwd)"
-git config user.name "${GITHUB_ACTOR}"
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-git fetch
-git pull
 
-# npm install
-echo "Setup npm"
-echo "//npm.pkg.github.com/:_authToken=${GITHUB_TOKEN}" | tee -a "${HOME}/.npmrc"
-git switch main
-npm ci
-git reset --hard
+function main() {
+  setup_git_ci
+  update_readme_gh_pages_branch
+  setup_npm_ci
 
-# update readme
-echo "Update readme"
-git switch main
-cp README.md /tmp/README.md
-git switch gh-pages
-cp -v /tmp/README.md README.md
-git add --verbose README.md
-git diff --quiet && git diff --staged --quiet || git commit -m "Update README.md for commit ${COMMIT}"
-git switch main
+  echo "Cleanup"
+  rm -rf charts/*
+  rm -rf dist/*
 
-# generated charts
-echo "Run typescript build"
-npx tsc
+  # generated charts
+  echo "Run typescript build"
+  npm ci
+  npx tsc
 
-git diff --quiet && git diff --staged --quiet || git commit -m "Build Helmcharts for commit ${COMMIT}"
+  for CHART in "${GENERATED_CHARTS[@]}"; do
+    echo "Processing chart: $CHART"
 
-for CHART in "${GENERATED_CHARTS[@]}"; do
-  echo "Processing chart: $CHART"
+    cd dist || exit 1
+    node "${CHART}.mjs"
 
-  cd dist && node "${CHART}.mjs"
-  bash set-version.sh "${CHART}" ./charts ./versions.yaml
+    PRE_HELMIFY_HOOK="pre_helmify_hook_${CHART}"
+    if declare -f "$PRE_HELMIFY_HOOK" > /dev/null; then
+      echo "Calling pre-helmify hook function: $PRE_HELMIFY_HOOK"
+      "$PRE_HELMIFY_HOOK"
+    else
+      echo "No pre-helmify hook function found for chart: $CHART"
+    fi
 
-  HOOK_FUNCTION="hook_${CHART}"
-  if declare -f "$HOOK_FUNCTION" > /dev/null; then
-    echo "Calling hook function: $HOOK_FUNCTION"
-    "$HOOK_FUNCTION"
-  else
-    echo "No hook function found for chart: $CHART"
-  fi
-done
+    helmify -vv --original-name -f "manifests/${CHART}" "${CHART}"
+    mkdir -pv "${SCRIPT_DIR}/charts/${CHART}"
+    cp -R "${CHART}"/* "${SCRIPT_DIR}/charts/${CHART}"
 
-function hook_crds() {
-  # no-op... for now
+    PRE_COMMIT_HOOK="pre_commit_hook_${CHART}"
+    if declare -f "$PRE_COMMIT_HOOK" > /dev/null; then
+      echo "Calling pre-commit hook function: $PRE_COMMIT_HOOK"
+      "$PRE_COMMIT_HOOK"
+    else
+      echo "No pre-commit hook function found for chart: $CHART"
+    fi
+
+    cd "${SCRIPT_DIR}" || exit 1
+    bash set-version.sh "${CHART}"
+    git add -v "charts/${CHART}"
+    git diff --quiet && git diff --staged --quiet || git commit -m "Build ${CHART} helmchart for commit ${COMMIT}"
+  done
+
+  for CHART in "${HANDWRITTEN_CHARTS[@]}"; do
+    echo "Processing chart: $CHART"
+
+    cd src || exit 1
+    mkdir -pv "${SCRIPT_DIR}/charts/${CHART}"
+    cp -R "${CHART}"/* "${SCRIPT_DIR}/charts/${CHART}"
+
+    PRE_COMMIT_HOOK="pre_commit_hook_${CHART}"
+    if declare -f "$PRE_COMMIT_HOOK" > /dev/null; then
+      echo "Calling pre-commit hook function: $PRE_COMMIT_HOOK"
+      "$PRE_COMMIT_HOOK"
+    else
+      echo "No pre-commit hook function found for chart: $CHART"
+    fi
+
+    cd "${SCRIPT_DIR}" || exit 1
+    bash set-version.sh "${CHART}"
+    git add -v "charts/${CHART}/*"
+    git diff --quiet && git diff --staged --quiet || git commit -m "Build ${CHART} helmchart for commit ${COMMIT}"
+  done
+
+  # Update deps in eevee chart
+  for CHART in "${GENERATED_CHARTS[@]} ${HANDWRITTEN_CHARTS[@]}"; do
+    echo "Updating version in eevee chart for ${CHART}"
+    CHART_VERSION=$(yq e ".${CHART}.chart" versions.yaml)
+    export CHART
+    export CHART_VERSION
+    yq e -i '(.dependencies[] | select(.name == env(CHART)) | .version) = env(CHART_VERSION)' "${SCRIPT_DIR}/charts/eevee/Chart.yaml"
+  done
+  git add -v "charts/eevee/*"
+  git diff --quiet && git diff --staged --quiet || git commit -m "Update deps of ${CHART} helmchart for commit ${COMMIT}"
 }
 
-function hook_operator() {
-  yq e -i '(.spec.selector.matchLabels, .spec.template.metadata.labels, .spec.selector) |= with_entries(select(.key == "cdk8s.io/metadata.addr") | .key = "eevee.bot/operator")' dist/manifests/operator/eevee-operator.yaml
-  yq e -i '(.. | select(tag == "!!map" and has("eevee.bot/operator"))) |= (.["eevee.bot/operator"] = "true")' dist/manifests/operator/eevee-operator.yaml
+function setup_git_ci() {
+  # git setup
+  echo "Setup git"
+  git config --global --add safe.directory "$(pwd)"
+  git config user.name "${GITHUB_ACTOR}"
+  git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+  git fetch
+  git pull
 }
+
+function update_readme_gh_pages_branch() {
+  # update readme
+  echo "Update readme"
+  git switch main
+  cp README.md /tmp/README.md
+  git switch gh-pages
+  cp -v /tmp/README.md README.md
+  git add --verbose README.md
+  git diff --quiet && git diff --staged --quiet || git commit -m "Update README.md for commit ${COMMIT}"
+  git switch main
+}
+
+function setup_npm_ci() {
+  # npm install
+  echo "Setup npm"
+  echo "//npm.pkg.github.com/:_authToken=${GITHUB_TOKEN}" | tee -a "${HOME}/.npmrc"
+  git switch main
+  npm ci
+}
+
+function pre_helmify_hook_operator() {
+  echo "noop"
+  # yq e -i '(.spec.selector.matchLabels, .spec.template.metadata.labels, .spec.selector) |= with_entries(select(.key == "cdk8s.io/metadata.addr") | .key = "eevee.bot/operator")' dist/manifests/operator/eevee-operator.yaml
+  # yq e -i '(.. | select(tag == "!!map" and has("eevee.bot/operator"))) |= (.["eevee.bot/operator"] = "true")' dist/manifests/operator/eevee-operator.yaml
+}
+
+main
